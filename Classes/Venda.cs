656b45fcec4107b8363cfe2391adcd75e65f454c
RAUL VEIGA DE ALVARENGA <45610136873@jpmorgan.sp>
2017-10-27 19:22:49 -0200

    Sumário da Classe Venda

--- Classes/Venda.cs
+++ Classes/Venda.cs
@@ -1,6 +1,9 @@
 using System;
 
 namespace CadastroVendasPoo.classes{
+    /// <summary>
+    /// A classe Vendas possui informações sobre a venda
+    /// </summary>
     public class Venda{
         private string cpf;
         public string Cpf
@@ -20,15 +23,23 @@ namespace CadastroVendasPoo.classes{
             get { return dataVenda;}
             set { dataVenda = value;}
         }
-        
+        /// <summary>
+        /// Constrói o objeto Venda
+        /// </summary>
         public Venda(){}
+        /// <summary>
+        /// Constrói o objeto Venda e você precisa passar os parâmetros
+        /// </summary>
+        /// <param name="id">Passe o id da venda como int</param>
+        /// <param name="cpf">Passe o cpf do cliente como string</param>
+        /// <param name="dataVenda">Passe a data da venda como DateTime</param>
         public Venda(int id, string cpf, DateTime dataVenda){
             this.id = id;
             this.cpf = cpf;
             this.dataVenda = dataVenda;
         }
-        
-        
+
+
         
         
     }
